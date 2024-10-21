862836eacd3d62114364d974fdf5b475919a1db7
Spror <wiktorspringer27@gmail.com>
2024-10-21 23:39:36 +0200

    feat: add sketch of class CameraControl

--- gesture_detection/include/gesture_detection/CameraControl.hpp
+++ gesture_detection/include/gesture_detection/CameraControl.hpp
@@ -4,5 +4,12 @@
 
 class CameraControl {
 public:
+    CameraControl() = delete;
+    CameraControl(int index = 0);
+    cv::Mat captureFrame();
+    bool isOpened();
+
 private:
+    cv::VideoCapture camera_;
+    const int index_;
 };
