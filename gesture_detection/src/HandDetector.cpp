58566c1c0ed5c9c6231176ef1b8f87b897e2a85b
Spror <wiktorspringer27@gmail.com>
2024-10-28 19:09:07 +0100

    style: code formatting - HandDetector.cpp

--- gesture_detection/src/HandDetector.cpp
+++ gesture_detection/src/HandDetector.cpp
@@ -1,20 +1,17 @@
 #include "HandDetector.hpp"
 
-
-cv::Mat HandDetector::detectHand(const cv::Mat& frame)
-{
+cv::Mat HandDetector::detectHand(const cv::Mat& frame) {
     auto hand = imageProcess(frame);
+
+    return hand;
 }
 
-
-cv::Mat HandDetector::imageProcess(const cv::Mat& frame)
-{
+cv::Mat HandDetector::imageProcess(const cv::Mat& frame) {
     cv::Mat hsv;
     cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
 
     cv::Mat skinMask;
     cv::inRange(hsv, lowerBound, upperBound, skinMask);
-
 
     cv::erode(skinMask, skinMask, cv::Mat(), cv::Point(-1, -1), 2);
     cv::dilate(skinMask, skinMask, cv::Mat(), cv::Point(-1, -1), 2);
