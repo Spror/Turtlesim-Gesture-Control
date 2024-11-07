172388293c8c759e13d9b82f224f38c247701169
Spror <wiktorspringer27@gmail.com>
2024-11-07 22:42:07 +0100

    feat: working on ConvexityDefectsDetection - pointDistanceOnX, pointDistance, calculateMedian, compressPointsByNeighborhood

--- gesture_detection/include/gesture_detection/ConvexityDefectsDetection.hpp
+++ gesture_detection/include/gesture_detection/ConvexityDefectsDetection.hpp
@@ -1,18 +1,31 @@
 #pragma once
+#include <array>
 #include <memory>
+#include <rclcpp/rclcpp.hpp>
 #include <vector>
 #include "GestureDetectionStrategy.hpp"
 #include "HandDetector.hpp"
 
+using pointsVec = std::vector<cv::Point>;
+using closestPointsArr = std::array<cv::Point, 2>;
 class ConvexityDefectsDetection : public GestureDetectionStrategy {
 public:
     ConvexityDefectsDetection() = delete;
-    explicit ConvexityDefectsDetection(cv::Scalar lower_bound, cv::Scalar upper_bound)
-        : handDetecotr_ptr_(std::make_unique<HandDetector>(lower_bound, upper_bound)) {};
+    explicit ConvexityDefectsDetection(const rclcpp::Logger& logger,
+                                       cv::Scalar lower_bound,
+                                       cv::Scalar upper_bound)
+        : logger_{logger},
+          handDetecotr_ptr_(std::make_unique<HandDetector>(lower_bound, upper_bound)) {};
 
-    Gesture gestureDetection(cv::Mat& frame) override;
+    Gesture gestureDetection(const cv::Mat& frame, cv::Mat& output) override;
 
 private:
+    const rclcpp::Logger logger_;
     std::unique_ptr<HandDetector> handDetecotr_ptr_;
 
+    inline double pointDistanceOnX(const cv::Point a, const cv::Point b) const;
+    inline double pointDistance(const cv::Point a, const cv::Point b) const;
+    cv::Point calculateMedian(const pointsVec& group) const;
+    closestPointsArr findClosestOnX(const pointsVec& points, const cv::Point pivot) const;
+    pointsVec compressPointsByNeighborhood(const pointsVec& points, const double maxDistance) const;
 };
--- gesture_detection/src/ConvexityDefectsDetection.cpp
+++ gesture_detection/src/ConvexityDefectsDetection.cpp
@@ -1,5 +1,126 @@
 #include "ConvexityDefectsDetection.hpp"
+#include <limits.h>
 
-Gesture ConvexityDefectsDetection::gestureDetection(cv::Mat& frame) {
+#define FINGER_SIZE_SCALING 0.3
+#define NEIGHBOR_DISTANCE_SCALING 0.1
 
+inline double ConvexityDefectsDetection::pointDistanceOnX(const cv::Point a,
+                                                          const cv::Point b) const {
+    return std::abs(a.x - b.x);
+}
+
+inline double ConvexityDefectsDetection::pointDistance(const cv::Point a, const cv::Point b) const {
+    return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
+}
+
+cv::Point ConvexityDefectsDetection::calculateMedian(const pointsVec& group) const {
+    cv::Point sum(0, 0);
+    for (const auto& point : group) {
+        sum += point;
+    }
+
+    return sum / static_cast<int>(group.size());
+}
+
+closestPointsArr ConvexityDefectsDetection::findClosestOnX(const pointsVec& points,
+                                                           const cv::Point pivot) const {
+    closestPointsArr closestPoints;
+
+    auto maxDistanceX = DBL_MAX;
+
+    return closestPoints;
+}
+
+Gesture ConvexityDefectsDetection::gestureDetection(const cv::Mat& frame, cv::Mat& output) {
+    if (frame.empty()) {
+        RCLCPP_ERROR(logger_, "Input frame is empty.");
+        return Gesture::No_gesture;
+    }
+
+    output = handDetecotr_ptr_->detectHand(frame);
+
+    if (output.empty()) {
+        RCLCPP_ERROR(logger_, "Hand extraction failure.");
+        return Gesture::No_gesture;
+    }
+
+    pointsVec hull;
+    std::vector<int> hull_ints;
+
+    auto contour = handDetecotr_ptr_->getHandContour();
+
+    cv::convexHull(contour, hull, true);
+    cv::convexHull(contour, hull_ints, false);
+
+    std::vector<cv::Vec4i> defects;
+
+    // At least 3 points to find defects
+    if (hull_ints.size() < 3) {
+        RCLCPP_ERROR(logger_, "Defects finding failure.");
+        return Gesture::No_gesture;
+    }
+
+    cv::convexityDefects(contour, hull_ints, defects);
+    cv::cvtColor(output, output, cv::COLOR_GRAY2BGR);
+
+    cv::Rect bounding_rectangle = cv::boundingRect(hull);
+    cv::rectangle(output, bounding_rectangle.tl(), bounding_rectangle.br(), cv::Scalar(255), 2, 8,
+                  0);
+
+    cv::Point center_bounding_rect((bounding_rectangle.tl().x + bounding_rectangle.br().x) / 2,
+                                   (bounding_rectangle.tl().y + bounding_rectangle.br().y) / 2);
+
+    pointsVec start_points;
+    pointsVec far_points;
+
+    for (const auto& defect : defects) {
+        start_points.push_back(contour[defect.val[0]]);
+
+        if (pointDistance(contour[defect.val[2]], center_bounding_rect) <
+            bounding_rectangle.height * FINGER_SIZE_SCALING) {
+            far_points.push_back(contour[defect.val[2]]);
+        }
+    }
+
+    start_points = compressPointsByNeighborhood(
+        start_points, bounding_rectangle.width * NEIGHBOR_DISTANCE_SCALING);
+
+    far_points = compressPointsByNeighborhood(far_points,
+                                              bounding_rectangle.width * NEIGHBOR_DISTANCE_SCALING);
+
+    for (const auto& point : start_points) {
+        cv::circle(output, point, 2, cv::Scalar(0, 0, 255), -1);
+    }
+
+    for (const auto& point : far_points) {
+        cv::circle(output, point, 2, cv::Scalar(0, 255, 0), -1);
+    }
+
+    return Gesture::No_gesture;
+}
+
+pointsVec ConvexityDefectsDetection::compressPointsByNeighborhood(const pointsVec& points,
+                                                                  const double maxDistance) const {
+    pointsVec median_points;
+
+    if (points.empty() || maxDistance <= 0)
+        return median_points;
+
+    pointsVec current_group;
+    current_group.push_back(points[0]);
+
+    for (size_t i = 1; i < points.size(); ++i) {
+        if (pointDistance(current_group.back(), points[i]) > maxDistance) {
+            median_points.push_back(calculateMedian(current_group));
+
+            current_group.clear();
+        }
+        current_group.push_back(points[i]);
+    }
+
+    if (!current_group.empty()) {
+        median_points.push_back(calculateMedian(current_group));
+    }
+
+    return median_points;
 }
