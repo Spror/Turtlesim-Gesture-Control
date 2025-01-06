45b2e21af3c63f092fc93617e9a8268827fd6538
Spror <wiktorspringer27@gmail.com>
2025-01-06 18:56:12 +0100

    feat: color bounds as a ROS2 parameter

--- gesture_detection/include/gesture_detection/GestureDetection.hpp
+++ gesture_detection/include/gesture_detection/GestureDetection.hpp
@@ -16,13 +16,20 @@ public:
     GestureDetection();
 
 private:
+    uint8_t finger_count_;
     void publishGesture();
+
+    rcl_interfaces::msg::SetParametersResult parametersCallback(
+        const std::vector<rclcpp::Parameter>& parameters);
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr publisher_;
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback_;
+
+    cv::Scalar lower_bound_;
+    cv::Scalar upper_bound_;
 
     std::shared_ptr<CameraControl> camera_p_;
+    std::shared_ptr<HandDetector> hand_detector_p_;
     std::shared_ptr<GestureDetectionStrategy> strategy_detection_p_;
-
-    uint8_t finger_count_;
 };
--- gesture_detection/src/GestureDetection.cpp
+++ gesture_detection/src/GestureDetection.cpp
@@ -1,30 +1,40 @@
 #include <GestureDetection.hpp>
+#include <algorithm>
 
-GestureDetection::GestureDetection() : Node("gesture_detecion_node"), finger_count_(0) {
-    this->declare_parameter<std::vector<uint8_t>>("lower_bound", {0, 0, 0});
-    this->declare_parameter<std::vector<uint8_t>>("upper_bound", {255, 255, 255});
+GestureDetection::GestureDetection() : Node("gesture_detection"), finger_count_(0) {
+    this->declare_parameter<std::vector<long>>("lower_bound", {0, 0, 0});
+    this->declare_parameter<std::vector<long>>("upper_bound", {255, 255, 255});
 
-    std::vector<uint8_t> lower_bound_vec, upper_bound_vec;
+    std::vector<long> lower_bound_vec, upper_bound_vec;
     this->get_parameter("lower_bound", lower_bound_vec);
     this->get_parameter("upper_bound", upper_bound_vec);
 
-    cv::Scalar lower_bound(static_cast<double>(lower_bound_vec[0]),
-                           static_cast<double>(lower_bound_vec[1]),
-                           static_cast<double>(lower_bound_vec[2]));
-    cv::Scalar upper_bound(static_cast<double>(upper_bound_vec[0]),
-                           static_cast<double>(upper_bound_vec[1]),
-                           static_cast<double>(upper_bound_vec[2]));
+    lower_bound_ =
+        cv::Scalar(static_cast<double>(lower_bound_vec[0]), static_cast<double>(lower_bound_vec[1]),
+                   static_cast<double>(lower_bound_vec[2]));
+    upper_bound_ =
+        cv::Scalar(static_cast<double>(upper_bound_vec[0]), static_cast<double>(upper_bound_vec[1]),
+                   static_cast<double>(upper_bound_vec[2]));
 
     publisher_ = this->create_publisher<std_msgs::msg::UInt8>("gesture_topic", 10);
+    hand_detector_p_ = std::make_shared<HandDetector>(lower_bound_, upper_bound_);
 
     camera_p_ = std::make_shared<CameraControl>(0);
     strategy_detection_p_ =
-        std::make_shared<ConvexityDefectsDetection>(this->get_logger(), lower_bound, upper_bound);
+        std::make_shared<ConvexityDefectsDetection>(this->get_logger(), hand_detector_p_);
+
+    param_callback_ = this->add_on_set_parameters_callback(
+        std::bind(&GestureDetection::parametersCallback, this, std::placeholders::_1));
 
     if (!camera_p_->isOpened()) {
         RCLCPP_ERROR(this->get_logger(), "Camera opening failure");
         rclcpp::shutdown();
     }
+
+    RCLCPP_INFO(this->get_logger(), "Initial lower_bound: [%f, %f, %f]", lower_bound_[0],
+                lower_bound_[1], lower_bound_[2]);
+    RCLCPP_INFO(this->get_logger(), "Initial upper_bound: [%f, %f, %f]", upper_bound_[0],
+                upper_bound_[1], upper_bound_[2]);
 
     timer_ = create_wall_timer(20ms, std::bind(&GestureDetection::publishGesture, this));
 }
@@ -48,4 +58,66 @@ void GestureDetection::publishGesture() {
     cv::imshow("Camera view", frame);
     cv::imshow("Processed view", hand);
     cv::waitKey(1);
+}
+
+rcl_interfaces::msg::SetParametersResult GestureDetection::parametersCallback(
+    const std::vector<rclcpp::Parameter>& parameters) {
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    result.reason = "Parameters updated successfuly.";
+
+    auto is_in_range = [](const auto& x) {
+        if (x >= 0 && x <= 255) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    for (const auto& param : parameters) {
+        if (param.get_name() == "lower_bound" &&
+            param.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER_ARRAY) {
+            auto new_values_array = param.as_integer_array();
+            if (new_values_array.size() == 3) {
+                if (std::all_of(begin(new_values_array), end(new_values_array), is_in_range)) {
+                    lower_bound_ = cv::Scalar(static_cast<uint8_t>(new_values_array[0]),
+                                              static_cast<uint8_t>(new_values_array[1]),
+                                              static_cast<uint8_t>(new_values_array[3]));
+                    hand_detector_p_->setLowerBound(lower_bound_);
+                    RCLCPP_INFO(this->get_logger(), "Updated lower_bound [%f, %f, %f]",
+                                lower_bound_[0], lower_bound_[1], lower_bound_[2]);
+                } else {
+                    result.successful = false;
+                    result.reason = "Invalid numbers for parameters.";
+                }
+            } else {
+                result.successful = false;
+                result.reason = "Invalid size for parameters.";
+            }
+        } else if (param.get_name() == "upper_bound" &&
+                   param.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER_ARRAY) {
+            auto new_values_array = param.as_integer_array();
+            if (new_values_array.size() == 3) {
+                if (std::all_of(begin(new_values_array), end(new_values_array), is_in_range)) {
+                    upper_bound_ = cv::Scalar(static_cast<uint8_t>(new_values_array[0]),
+                                              static_cast<uint8_t>(new_values_array[1]),
+                                              static_cast<uint8_t>(new_values_array[3]));
+                    hand_detector_p_->setUpperBound(upper_bound_);
+                    RCLCPP_INFO(this->get_logger(), "Updated lower_bound [%f, %f, %f]",
+                                upper_bound_[0], upper_bound_[1], upper_bound_[2]);
+                } else {
+                    result.successful = false;
+                    result.reason = "Invalid numbers for parameters.";
+                }
+            } else {
+                result.successful = false;
+                result.reason = "Invalid size for parameters.";
+            }
+        } else {
+            result.successful = false;
+            result.reason = "Invalid parameters.";
+        }
+    }
+
+    return result;
 }
